3d96ba9df433944bfa16a8cfa7e2480836e3d282
trulynolan <nolanpedersen@gmail.com>
2021-10-14 17:58:08 -0400

    finished_assignment

--- Algorithms/Week_1.js
+++ Algorithms/Week_1.js
@@ -59,6 +59,54 @@ console.log("there are " + countPositives + " positive values");
 
 
 
+// Week 1 Day 4
+
+var x = 5;
+    
+function setX(newValue) {
+    x = newValue; 
+}
+    
+console.log(x);
+setX(15);
+console.log(x);
+
+5, 15
+
+
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    // The moment you reach "return," nothing else in the line matters. Break the function! There is a value returned no matter what. 
+    console.log("hello there");
+}
+    
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
+
+// 5, -5, 5
+
+
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+// for (var left=0; left<5/2 left++)
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+    
+var result1 = isPal( [1, 1, 2, 2, 1] );
+console.log(result1);
+    
+var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+console.log(result2);
+
 
 
 
