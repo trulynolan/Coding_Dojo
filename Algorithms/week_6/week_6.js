273b4c8a660d3537a58bbd71ab8f6fbe4b126efe
trulynolan <nolanpedersen@gmail.com>
2021-11-18 11:37:20 -0500

    algos

--- Algorithms/week_6/week_6.js
+++ Algorithms/week_6/week_6.js
@@ -71,3 +71,46 @@ function recursiveGreatestCommonFactor(num1,num2){
   console.log(recursiveGreatestCommonFactor(123456,987654)); // 6
   console.log(recursiveGreatestCommonFactor(102,2000)); // 2
   console.log(recursiveGreatestCommonFactor(7,13)); // 1
+
+
+
+// # Finding An Average on the Down Low
+
+// ### Three Employees want to know the average of their salaries. They are not allowed to share their individual salaries.
+// * They cannot directly share their own salaries in anyway, which includes writing things down. 
+
+
+// =================================
+
+// # Card Flip Shuffle #
+
+// ## You are in a dark room with a deck of cards. A certain number of the cards are face up and the rest are face down. You can't see the cards. How do you divide the deck in to two piles with equal numbers of face up cards in each? ##
+
+// * You know how many cards are flipped, but you cannot see the deck of cards no matter what and the deck is shuffled, so the flipped cards could be anywhere in the deck.
+
+
+// =================================
+
+// Word/Logic Problems!
+
+// Vampires and the sleeping man
+
+// Consider a village with vampires and a sleeping man (who never wakes up, no matter what).
+// A vampire can eat the sleeping man, but after eating him, the vampire falls asleep.
+// Similarly, any vampire can eat any other sleeping vampire, and this process repeats.
+// Assume that the vampires are very smart and would ALWAYS choose to stay alive than to eat the man and risk their lives.
+// Initially, there are 65 vampires and 1 sleeping man. What would happen in the village?
+
+
+// =================================
+
+// Bonus riddle:
+
+// John gave his brother James a box:
+// About it there were many locks.
+// James woke and said it gave him pain;
+// So gave it back to John again.
+// The box was not with lid supplied
+// Yet caused two lids to open wide:
+// And all these locks had never a key
+// What kind of box, then, could it be?
