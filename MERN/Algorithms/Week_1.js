c02e4c50767300bfebdaf147acb79a0bfe1e01ae
trulynolan <nolanpedersen@gmail.com>
2022-01-10 11:06:50 -0500

    algos

--- MERN/Algorithms/Week_1.js
+++ MERN/Algorithms/Week_1.js
@@ -12,7 +12,7 @@ function bubbleSort(arr){
                 var temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
-            }
+            } 
         }
     }
     
