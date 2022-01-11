d9dab93328f6b6905181ac24c7329b98082a4e93
trulynolan <nolanpedersen@gmail.com>
2022-01-11 11:34:23 -0500

    algo update

--- MERN/Algorithms/Week_1.js
+++ MERN/Algorithms/Week_1.js
@@ -82,3 +82,41 @@ const combine = (leftArr, rightArr) => {
 }
 
 console.log(combine([1,2,7,9],[0,3,4,6,11]));
+
+const combine = (leftArr, rightArr) => {
+    console.log(leftArr);
+    let left = 0;
+    let right = 0;
+    let newArr = [];
+    while(left < leftArr.length || right < rightArr.length){
+        if(left >= leftArr.length){
+            newArr.push(rightArr[right]);
+            ++right;
+        }
+        else if(right >= rightArr.length){
+            newArr.push(leftArr[left]);
+            ++left;
+        }
+        else if(left < leftArr.length && leftArr[left] < rightArr[right]){
+            newArr.push(leftArr[left]);
+            ++left;
+        }
+        else{
+            newArr.push(rightArr[right]);
+            ++right;
+        }
+    }
+    return newArr;
+}
+
+const mergeSort = (arr) => {
+    if(arr.length < 2){
+        return arr;
+    }
+    const left = arr.slice(0, arr.length/2);
+    const right = arr.slice(arr.length/2, arr.length);
+    return combine(mergeSort(left), mergeSort(right));
+}
+
+//should return [1,2,3,4,5,6,8,14]
+console.log(mergeSort([5,4,2,6,8,14,1,3,11]));
