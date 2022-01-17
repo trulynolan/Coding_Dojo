b1fa6cf566c78717ba2f92b7fbb7dd2f265a8d4f
trulynolan <nolanpedersen@gmail.com>
2022-01-16 19:03:10 -0500

    assignments

---
+++ MERN/fundamentals/firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Eat cheese</li>
+        <li>Drink a brewski</li>
+        <li>Play among us with Vee</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/fundamentals/prop_it/src/components/PersonCard.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component{
+    
+    constructor(props){
+    super(props);
+
+    this.state = {
+        age: this.props.age
+    };
+}
+
+
+
+    updateAge = () =>{
+        this.setState({age: this.state.age + 1});
+    };
+    render(){
+        return(
+            <div>
+                <h1> {this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick = {this.updateAge}>Birthday button for {this.props.firstName}{this.props.lastName}</button>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
